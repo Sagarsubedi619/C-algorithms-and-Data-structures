7bebf0dd7c9aab7746b4573790aaaf85a25f96cc
Sagar Subedi <59858676+Sagarsubedi619@users.noreply.github.com>
2022-01-28 17:22:24 -0600

    Update deque_node.h

--- Programs/assignment2/deque_node.h
+++ Programs/assignment2/deque_node.h
@@ -1,9 +1,29 @@
+#ifndef H_deque_node
+#define H_deque_node
+
 #include <iostream>
 using namespace std;
 
 template <class T>
 struct deque_node
 {
-    T *Next;
-    T *Previous;
+    T data; //datafield
+     struct deque_node *next; //next pointer
+     struct deque_node *prev; //previous pointer
+
+
+     static deque_node* getnode(const T& data)
+     {
+         deque_node* newNode=(deque_node*)malloc(sizeof(deque_node));
+         newNode->data=data;
+         newNode->prev=newNode->next=NULL;
+         return newNode;
+
+         //get new node in the heap
+     }
+    
+    
 };
+
+#endif
+
